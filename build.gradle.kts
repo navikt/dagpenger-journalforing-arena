450fb28ba65ad02aa28bbc7bc6b3203f966c0379
Geir André Lund <geir.andre.lund@nav.no>
2019-01-18 12:27:41 +0100

    Update kafka-embedded-env to 2.0.1 - removes dependency to kotlin 1.2.x

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,7 @@ dependencies {
     testImplementation("junit:junit:4.12")
     testImplementation("org.mockito:mockito-all:2.+")
     testImplementation("com.github.tomakehurst:wiremock:2.19.0")
-    testImplementation("no.nav:kafka-embedded-env:2.0.1")
+    testImplementation("no.nav:kafka-embedded-env:2.0.2")
 }
 
 spotless {
