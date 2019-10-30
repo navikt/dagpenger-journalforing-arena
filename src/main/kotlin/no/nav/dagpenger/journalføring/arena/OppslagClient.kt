48c9ea97f3c10ce7ae9e80c5d9dc44b208f47938
Geir André Lund <geir.andre.lund@nav.no>
2019-10-30 13:34:02 +0100

    - Bring gradle config in from monorepo (dependencies) - Deploy with naiserator - Enable vault path for service user credentials - Remove old code
    resolves navikt/dagpenger#248

--- src/main/kotlin/no/nav/dagpenger/journalføring/arena/OppslagClient.kt
+++
@@ -1,6 +0,0 @@
-package no.nav.dagpenger.journalføring.arena
-
-interface OppslagClient {
-    fun createOppgave(request: CreateArenaOppgaveRequest): String
-    fun getSaker(request: GetArenaSakerRequest): List<ArenaSak>
-}
