c95ee76b09b40703462ed590f54c402a8d350b44
Geir André Lund <geir.andre.lund@nav.no>
2019-11-05 13:26:42 +0100

    Fix deloy

--- src/main/kotlin/no/nav/dagpenger/journalføring/arena/JournalføringArena.kt
+++ src/main/kotlin/no/nav/dagpenger/journalføring/arena/JournalføringArena.kt
@@ -3,10 +3,6 @@ package no.nav.dagpenger.journalføring.arena
 import mu.KotlinLogging
 import no.nav.dagpenger.events.Packet
 import no.nav.dagpenger.journalføring.arena.adapter.ArenaOppgaveClient
-import no.nav.dagpenger.journalføring.arena.adapter.soap.SoapArenaOppgaveClient
-import no.nav.dagpenger.journalføring.arena.adapter.soap.SoapPort
-import no.nav.dagpenger.journalføring.arena.adapter.soap.configureFor
-import no.nav.dagpenger.journalføring.arena.adapter.soap.stsClient
 import no.nav.dagpenger.streams.River
 import no.nav.dagpenger.streams.streamConfig
 import org.apache.kafka.streams.kstream.Predicate
@@ -62,19 +58,19 @@ fun main(args: Array<String>) {
     val configuration = Configuration()
 
     val service = if (configuration.application.profile != Profile.PROD) {
-        val behandleArbeidsytelseSak =
-            SoapPort.BehandleArbeidOgAktivitetOppgaveV1(configuration.behandleArbeidsytelseSak.endpoint)
-
-        val arenaOppgaveClient: ArenaOppgaveClient =
-            SoapArenaOppgaveClient(behandleArbeidsytelseSak)
-
-        val soapStsClient = stsClient(
-            stsUrl = configuration.soapSTSClient.endpoint,
-            credentials = configuration.soapSTSClient.username to configuration.soapSTSClient.password
-        ).also {
-            it.configureFor(behandleArbeidsytelseSak)
-        }
-        JournalføringArena(configuration, arenaOppgaveClient)
+        // val behandleArbeidsytelseSak =
+        //     SoapPort.BehandleArbeidOgAktivitetOppgaveV1(configuration.behandleArbeidsytelseSak.endpoint)
+        //
+        // val arenaOppgaveClient: ArenaOppgaveClient =
+        //     SoapArenaOppgaveClient(behandleArbeidsytelseSak)
+        //
+        // val soapStsClient = stsClient(
+        //     stsUrl = configuration.soapSTSClient.endpoint,
+        //     credentials = configuration.soapSTSClient.username to configuration.soapSTSClient.password
+        // ).also {
+        //     it.configureFor(behandleArbeidsytelseSak)
+        // }
+        JournalføringArena(configuration, DummyArenaOppgaveClient())
     } else {
         JournalføringArena(configuration, DummyArenaOppgaveClient())
     }
