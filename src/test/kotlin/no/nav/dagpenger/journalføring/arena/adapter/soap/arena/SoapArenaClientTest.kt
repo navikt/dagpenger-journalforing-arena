e7c25fd128bf9df9d315483ef831996bdd9773d5
Geir André Lund <geir.andre.lund@nav.no>
2019-11-14 08:21:03 +0100

    Added tests

--- src/test/kotlin/no/nav/dagpenger/journalføring/arena/adapter/soap/arena/SoapArenaClientTest.kt
+++ src/test/kotlin/no/nav/dagpenger/journalføring/arena/adapter/soap/arena/SoapArenaClientTest.kt
@@ -3,6 +3,7 @@ package no.nav.dagpenger.journalføring.arena.adapter.soap.arena
 import io.kotlintest.shouldBe
 import io.mockk.every
 import io.mockk.mockk
+import no.nav.dagpenger.streams.HealthStatus
 import no.nav.tjeneste.virksomhet.behandlearbeidogaktivitetoppgave.v1.BehandleArbeidOgAktivitetOppgaveV1
 import no.nav.tjeneste.virksomhet.behandlearbeidogaktivitetoppgave.v1.meldinger.WSBestillOppgaveResponse
 import no.nav.tjeneste.virksomhet.ytelseskontrakt.v3.YtelseskontraktV3
@@ -39,5 +40,27 @@ internal class SoapArenaClientTest {
         saker.isEmpty() shouldBe false
         saker.first().fagsystemSakId shouldBe 123
         saker.first().status shouldBe "INAKT"
+    }
+
+    @Test
+    fun ` helsesjekk skal være ok når Arena er oppe`() {
+        val ytelseskontraktV3: YtelseskontraktV3 = mockk()
+        every {
+            ytelseskontraktV3.ping()
+        } returns Unit
+
+        val client = SoapArenaClient(mockk(), ytelseskontraktV3)
+        client.status() shouldBe HealthStatus.UP
+    }
+
+    @Test
+    fun ` helsesjekk skal ikke være ok når Arena nede`() {
+        val ytelseskontraktV3: YtelseskontraktV3 = mockk()
+        every {
+            ytelseskontraktV3.ping()
+        } throws RuntimeException()
+
+        val client = SoapArenaClient(mockk(), ytelseskontraktV3)
+        client.status() shouldBe HealthStatus.DOWN
     }
 }
