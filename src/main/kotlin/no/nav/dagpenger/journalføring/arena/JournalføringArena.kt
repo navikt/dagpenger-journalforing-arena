69f922dbd7a1a8c767aa4f6fb8c1f5dbba4ee248
Andreas Bergh <andreas.nygard.bergh@nav.no>
2019-11-29 10:55:20 +0100

    spotless apply

--- src/main/kotlin/no/nav/dagpenger/journalføring/arena/JournalføringArena.kt
+++ src/main/kotlin/no/nav/dagpenger/journalføring/arena/JournalføringArena.kt
@@ -70,7 +70,6 @@ class JournalføringArena(
             packet.getObjectValue(PacketKeys.BEHANDLENDE_ENHETER) { behandlendeenhetAdapter.fromJsonValue(it)!! }
                 .first().enhetId
 
-
         val saker = arenaClient.hentArenaSaker(naturligIdent)
 
         val fakta =
