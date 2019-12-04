826ed8d3502b5b789d5a260408099d51f63c7036
Andreas Bergh <andreas.nygard.bergh@nav.no>
2019-12-04 08:56:08 +0100

    Oppdater test til å faktisk bruke ekte json

--- src/test/kotlin/no/nav/dagpenger/journalføring/arena/DokumentAdapterTest.kt
+++ src/test/kotlin/no/nav/dagpenger/journalføring/arena/DokumentAdapterTest.kt
@@ -1,26 +1,17 @@
 package no.nav.dagpenger.journalføring.arena
 
-import com.squareup.moshi.Types
 import io.kotlintest.shouldBe
 import io.kotlintest.shouldNotBe
-import no.nav.dagpenger.events.moshiInstance
 import org.junit.jupiter.api.Test
 
 class DokumentAdapterTest {
     data class DokumentInfo(val tittel: String, val brevkode: String)
     @Test
     fun `Skal greie å konvertere dokumentInfo med flere felter til Dokument med tittel`() {
-        val dokumenter = listOf(DokumentInfo("en", "123"), DokumentInfo("to", "123"))
-        val dokumentInfoAdapter = moshiInstance.adapter<List<DokumentInfo>>(
-            Types.newParameterizedType(
-                List::class.java,
-                DokumentInfo::class.java
-            )
-        )
 
-        val json = dokumentInfoAdapter.toJsonValue(dokumenter)
+        val json = """[{"tittel": "en", "brevkode": "123"}, {"tittel": "to"}]""".trimIndent()
 
-        val konverterteDokumenter = dokumentAdapter.fromJsonValue(json)
+        val konverterteDokumenter = dokumentAdapter.fromJson(json)
 
         konverterteDokumenter shouldNotBe null
         konverterteDokumenter?.first()?.tittel shouldBe "en"
