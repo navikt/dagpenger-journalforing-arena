245a3b78c7e46608a6a09471c7f32d9aad2efb72
Andreas Bergh <andreas.nygard.bergh@nav.no>
2019-12-05 11:39:56 +0100

    Oppdaterer beskrivelsen til bestillOppgave
    Brukte tidligere feil tegn og start vedtak i stedet for start
    vedtaksbehandling

--- src/main/kotlin/no/nav/dagpenger/journalføring/arena/adapter/soap/arena/SoapArenaClient.kt
+++ src/main/kotlin/no/nav/dagpenger/journalføring/arena/adapter/soap/arena/SoapArenaClient.kt
@@ -41,7 +41,7 @@ class SoapArenaClient(private val oppgaveV1: BehandleArbeidOgAktivitetOppgaveV1,
             }
             frist = DatatypeFactory.newInstance().newXMLGregorianCalendar(GregorianCalendar.from(today))
             sakInfo = WSSakInfo().withTvingNySak(true)
-            beskrivelse = "Start vedtak – automatisk journalført.\n"
+            beskrivelse = "Start vedtaksbehandling - automatisk journalført.\n"
             this.tilleggsinformasjon = tilleggsinformasjon
         }
 
